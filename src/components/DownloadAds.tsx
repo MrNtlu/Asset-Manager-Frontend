641ee5c3b59b1d2583d6e82d65729756fe5eecb8
MrNtlu <97burakfidan97@gmail.com>
2022-06-22 14:43:55 +0300

    Images changed. iOS link added.

--- src/components/DownloadAds.tsx
+++ src/components/DownloadAds.tsx
@@ -5,7 +5,7 @@ function DownloadAds() {
     return (
         <div className="download">
             <div className="download_images flex">
-                <a href='#soon'><img src={require("../img/app_store.png")} alt="" className={downloadImgStyle + ` mr-[1.6rem]`}/></a>
+                <a href="https://apps.apple.com/tr/app/kanma-personal-finance/id1629419797"><img src={require("../img/app_store.png")} alt="" className={downloadImgStyle + ` mr-[1.6rem]`}/></a>
                 <a href='https://play.google.com/store/apps/details?id=com.mrntlu.kantan'><img src={require("../img/google_play.png")} alt="" className={downloadImgStyle}/></a>
             </div>
         </div>
