80b47e03b054d0a8459b37c4b3a61df86b603477
MrNtlu <97burakfidan97@gmail.com>
2022-06-29 14:43:24 +0300

    Minor design changes.

--- src/components/Descriptions.tsx
+++ src/components/Descriptions.tsx
@@ -6,7 +6,7 @@ function Descriptions() {
         <div className="flex flex-col items-center justify-start px-[5rem] p-[5rem] relative z-[2] pt-[7rem] bg-[#FFFFFF]">
             <h2 className="pt-10 text-[5rem] text-black">Features</h2>
             <div className="headline flex flex-col text-center items-center text-[1.4rem] mt-[0.5rem] text-black">
-                <span>Manager your money and track your investments and subscriptions</span>
+                <span>Manage your money and track your investments and subscriptions</span>
             </div>
             <div className="feature mt-[5rem] grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 lg:gap-0 gap-6">
                 <Feature icon="investment" title="Investments" description="Track all your investments in one app."/>
